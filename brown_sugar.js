1cf2eeade4f0f375eecf0c163ac02218d80e82e1
Ari <133548390+Em776102@users.noreply.github.com>
2026-02-18 14:32:39 -0500

    Update brown_sugar.js

--- brown_sugar.js
+++ brown_sugar.js
@@ -3,13 +3,17 @@ const elements = {};
 elements.brown_sugar = {
     color: "#B87333",
     behavior: behaviors.POWDER,
-    category: "powders",
+    category: "food",
     state: "solid",
     density: 1150,
     tempHigh: 100,
     stateHigh: "caramel",
     tempLow: 500,
     stateLow: "ash",
+    viscosity: 1,
+    stain: 0,
+    hidden: false,
+    isFood: true
 };
 
 elements.brown_sugar.reactions = {};
